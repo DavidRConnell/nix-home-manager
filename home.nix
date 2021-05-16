37cb5af9235aad3bc09937ca44507accb826fa26
David R Connell <davidconnell12@gmail.com>
2021-05-15 23:01:51 -0500

    Clean: move from configuration.nix to home.nix
    - Move a bunch of stuff initially thrown into global configuration.nix
    into the user configuration home.nix
    -
    -

--- home.nix
+++ home.nix
@@ -11,30 +11,40 @@
 
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+      url =
+        "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
     }))
   ];
 
   home.packages = with pkgs; [
-    zsh
-    redshift
     alacritty
-    tmux
-    vim
-    zip
-    unzip
-    htop
-    rsync
-    ripgrep
-    wget
-    direnv
-    fzf
-    scrot
-    zathura
+    (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
     binutils
-    w3m
+    fzf
+    git
+    htop
+    neovim
     nixfmt
+    pass
+    qutebrowser
+    rclone
+    redshift
+    restic
+    ripgrep
+    rsync
+    scrot
+    stow
+    shfmt
     sqlite
+    tectonic
+    tmux
+    unzip
+    w3m
+    wget
+    wordnet
+    zathura
+    zip
+    zsh
 
     (makeDesktopItem {
       name = "org-protocol";
@@ -47,24 +57,35 @@
   ];
 
   # https://github.com/rasendubi/dotfiles#emacs
-  programs = {
-    emacs = {
-      enable = true;
-      package = import ./emacs.nix { inherit pkgs; };
-    };
+  programs.emacs = {
+    enable = true;
+    package = import ./emacs.nix { inherit pkgs; };
   };
-  #     extraPackages = epkgs: with epkgs.melpaPackages; [
-    #     ];
-    #   };
-    # };
 
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    home.stateVersion = "21.03";
+  services.emacs.enable = true;
+  services.redshift = {
+    enable = true;
+    provider = "geoclue2";
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableNixDirenvIntegration = true;
+  };
+
+  home.sessionVariables = {
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_DATA_HOME = "$HOME/.local/share";
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.03";
 }
