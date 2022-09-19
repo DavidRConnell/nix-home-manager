584aed09fe75d1de458d58ab1a17636c1a1fca89
David R Connell <davidconnell12@gmail.com>
2022-09-19 12:41:53 -0500

    Add: gtk settings

---
+++ modules/gtk.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }: {
+
+  gtk = {
+    enable = true;
+    gtk2 = {
+      configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+      extraConfig = ''
+        gtk-recent-files-max-age=0
+      '';
+    };
+    gtk3 = {
+      extraConfig = {
+        gtk-recent-files-max-age = 0;
+        gtk-recent-files-limit = 0;
+      };
+    };
+    gtk4 = { extraConfig = { gtk-recent-files-enabled = false; }; };
+  };
+
+}
