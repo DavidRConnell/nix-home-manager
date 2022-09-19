803e1c065910099e9b80a43529844e17f5cdcbf7
David R Connell <davidconnell12@gmail.com>
2022-09-19 12:42:32 -0500

    Fix: make sure to get my version of emacs

--- modules/mail.nix
+++ modules/mail.nix
@@ -1,14 +1,12 @@
 { config, pkgs, lib, ... }: {
-  home.packages = (with pkgs; [
-    isync
-    mu
-  ]);
+  home.packages = (with pkgs; [ isync mu ]);
 
   services.mbsync = {
     enable = true;
     verbose = false;
     frequency = "*:0/15";
     # emacs locks mu so mu index will fail.
-    postExec = "${pkgs.emacs}/bin/emacsclient -e '(mu4e-update-index)'";
+    postExec =
+      "${config.programs.emacs.finalPackage}/bin/emacsclient -e '(mu4e-update-index)'";
   };
 }
