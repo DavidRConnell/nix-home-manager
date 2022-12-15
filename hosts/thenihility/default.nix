7cab85ab5891bcb3e80792bcc547c2fd8c57a558
David R Connell <davidconnell12@gmail.com>
2022-12-14 21:03:25 -0600

    Fix cachix setup

--- hosts/thenihility/default.nix
+++ hosts/thenihility/default.nix
@@ -1,11 +1,19 @@
 { pkgs, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix /etc/nixos/cachix.nix ];
+  imports = [ ./hardware-configuration.nix ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.device = "/dev/sda";
+  nix.settings = {
+    substituters =
+      [ "https://cache.nixos.org" "https://nix-community.cachix.org" ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   system.stateVersion = "20.09";
 
