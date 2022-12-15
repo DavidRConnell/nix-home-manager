75df0fcd2c69a1b479ec36477e4fff959064de6a
David R Connell <davidconnell12@gmail.com>
2022-12-15 01:55:58 -0600

    Update stumpwm

--- modules/host/desktop.nix
+++ modules/host/desktop.nix
@@ -1,12 +1,14 @@
 { ... }:
 
 {
+  imports = [ ./stumpwm.nix ];
   services.xserver = {
     enable = true;
     layout = "dvorak";
     xkbOptions = "ctrl:nocaps";
     displayManager.lightdm.enable = true;
     desktopManager.gnome.enable = true;
-    windowManager.stumpwm.enable = true;
+    # windowManager.stumpwm.enable = true;
+    windowManager.stumpwm-custom.enable = true;
   };
 }
--- modules/host/stumpwm.nix
+++ modules/host/stumpwm.nix
@@ -1,7 +1,5 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
   cfg = config.services.xserver.windowManager.stumpwm-custom;
   sbcl = "${pkgs.sbcl}/bin/sbcl --script";
@@ -28,107 +26,34 @@ let
 
   clx = pkgs.lispPackages_new.build-asdf-system {
     pname = "clx";
-    version = "20220808-git";
-    src = pkgs.fetchFromGitHub {
-      owner = "sharplispers";
-      repo = "clx";
-      rev = "master";
-      hash = "sha256-Dgo/oOa3xP3HnXWlwgUBfhYR8HRFrg1ud1iqb2JETG4=";
+    version = "0.7.5";
+    src = pkgs.fetchzip {
+      url =
+        "https://github.com/sharplispers/clx/archive/refs/tags/0.7.5.tar.gz";
+      hash = "sha256-jouNhlB7NkxNi65m+OGBCjBG6+duuco6ybnIC9M/Ju4=";
     };
     lisp = sbcl;
   };
 
-  # clx-truetype = pkgs.lispPackages_new.build-asdf-system {
-  #   pname = "clx-truetype";
-  #   version = "20160701-git";
-  #   src = pkgs.fetchFromGitHub {
-  #     owner = "l04m33";
-  #     repo = "clx-truetype";
-  #     rev = "master";
-  #     hash = "sha256-Z8giQ6rW8E/DoNY4RTz5C8cVLc+6T7iMdW1KJtL1MB0=";
-  #   };
-  #   lisp = sbcl;
-  #   lispLibs = [ clx ];
-  # };
-
-  # slynk = pkgs.lispPackages_new.build-asdf-system {
-  #   pname = "slynk";
-  #   version = "20221108-git";
-  #   src = pkgs.fetchurl {
-  #     url = "https://github.com/joaotavora/sly/tree/master/slynk";
-  #     hash = "sha256-UxlXtMXhi4WTc0/xvKnTkB7sbsuezN2Zq7ZEhGjDf3w=";
-  #   };
-  #   lisp = sbcl;
-  # };
-
-  stumpwm = pkgs.lispPackages_new.build-asdf-system {
+  stumpwm = (pkgs.lispPackages_new.build-asdf-system rec {
     pname = "stumpwm";
     version = "22.11";
     src = pkgs.fetchzip {
       url = "https://github.com/stumpwm/stumpwm/archive/refs/tags/22.11.tar.gz";
       hash = "sha256-zXj17ucgyFhv7P0qEr4cYSVRPGrL1KEIofXWN2trr/M=";
     };
-
     lisp = sbcl;
-    lispLibs = [
-      alexandria
-      cl-ppcre
-      clx
-      # clx-truetype
-      #slynk
-    ];
-  };
-
-  # stumpwm = pkgs.stdenvNoCC.mkDerivation rec {
-  #   pname = "stumpwm";
-  #   version = "22.11";
-
-  #   description = "A tiling, keyboard driven window manager";
-
-  #   buildInputs = with pkgs; [ sbcl gnumake bash autoconf texinfo ];
-  #   propagatedBuildInputs = with pkgs.lispPackages; [
-  #     alexandria
-  #     cl-ppcre
-  #     clx
-  #     clx-truetype
-  #     slynk
-  #   ];
-
-  #   src = pkgs.fetchurl {
-  #     url =
-  #       "https://github.com/stumpwm/${pname}/archive/refs/tags/${version}.tar.gz";
-  #     sha256 = "09phbrgcaq3k7dyvs79dcm6gi0zs7h8899ykm0cl598v5hxhz51n";
-  #   };
-
-  #   configurePhase = ''
-  #     runHook preConfigure
-
-  #     export SBCL_HOME=${sbcl}/bin
-  #     ./autogen.sh
-  #     ./configure
-
-  #     runHook postConfigure
-  #   '';
-
-  #   buildPhase = ''
-  #     make
-  #   '';
-
-  #   installPhase = ''
-  #     mkdir "$out"/bin
-  #     cp stumpwm "$out"/bin
-  #   '';
-  # };
-
+    lispLibs = [ alexandria cl-ppcre clx ];
+  });
 in {
   options = {
     services.xserver.windowManager.stumpwm-custom.enable =
-      mkEnableOption (lib.mdDoc "stumpwm-custom");
+      lib.mkEnableOption (lib.mdDoc "stumpwm-custom");
   };
 
-  config = mkIf cfg.enable {
-    services.xserver.windowManager.session = singleton {
-      name = "stumpwm";
+  config = lib.mkIf cfg.enable {
+    services.xserver.windowManager.session = lib.singleton {
+      name = "stumpwm-custom";
       start = ''
         ${stumpwm}/bin/stumpwm &
         waitPID=$!
