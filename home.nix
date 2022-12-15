1c253807ac02c19b4df3d124f75b54a9c2125187
David R Connell <davidconnell12@gmail.com>
2022-12-14 21:02:08 -0600

    Add services

--- home.nix
+++ home.nix
@@ -73,10 +73,52 @@
   ]);
 
   manual.manpages.enable = true;
+  programs.info.enable = true;
+  fonts.fontconfig.enable = true;
+
+  systemd.user.targets.tray = {
+    # Needed for some services that require tray.
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = [ "graphical-session-pre.target" ];
+    };
+  };
 
   services.redshift = {
     enable = true;
     provider = "geoclue2";
+  };
+
+  services.udiskie = {
+    enable = true;
+    notify = false;
+  };
+
+  services.unclutter = {
+    enable = true;
+    timeout = 3;
+    extraOptions = [ "ignore-scrolling" "exclude-root" ];
+  };
+
+  services.flameshot = {
+    enable = true;
+    settings = {
+      General = {
+        disabledTrayIcon = true;
+        showStartupLaunchMessage = false;
+      };
+      Shortcuts = {
+        TYPE_COPY = "Y";
+        TYPE_MOVE_DOWN = "J";
+        TYPE_MOVE_LEFT = "H";
+        TYPE_MOVE_RIGHT = "L";
+        TYPE_MOVE_UP = "K";
+        TYPE_RESIZE_DOWN = "Shift+J";
+        TYPE_RESIZE_LEFT = "Shift+H";
+        TYPE_RESIZE_RIGHT = "Shift+L";
+        TYPE_RESIZE_UP = "Shift+K";
+      };
+    };
   };
 
   programs.direnv = {
