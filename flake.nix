dcb8d1df5a27d3ef6a29eb3c87e651fc549cc152
David R Connell <davidconnell12@gmail.com>
2022-12-15 02:09:46 -0600

    Updating to 22.11

--- flake.nix
+++ flake.nix
@@ -6,14 +6,14 @@
       type = "github";
       owner = "NixOS";
       repo = "nixpkgs";
-      ref = "nixos-22.05";
+      ref = "nixos-22.11";
     };
 
     home-manager = {
       type = "github";
       owner = "nix-community";
       repo = "home-manager";
-      ref = "release-22.05";
+      ref = "release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
