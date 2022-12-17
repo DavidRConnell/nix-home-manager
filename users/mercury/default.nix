152dc2071c861672f7d8c3cc7a2cb2c63a6f96ed
David R Connell <davidconnell12@gmail.com>
2022-12-16 20:57:47 -0600

    Cut excess from mercuy's module

--- users/mercury/default.nix
+++ users/mercury/default.nix
@@ -6,15 +6,9 @@
   home = rec {
     username = "mercury";
     homeDirectory = "/home/${username}";
-    packages = with pkgs; [ htop yt-dlp protonvpn-cli losslesscut-bin ];
+    packages = with pkgs; [ htop yt-dlp losslesscut-bin ];
     stateVersion = "22.11";
   };
 
   programs.home-manager.enable = true;
-
-  services.gpg-agent = {
-    enable = true;
-    defaultCacheTtl = 3600;
-    enableSshSupport = true;
-  };
 }
