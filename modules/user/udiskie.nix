081e3fe0892856e95863654400651b7d17d0a092
David R Connell <davidconnell12@gmail.com>
2023-01-11 14:10:03 -0600

    Set up a fake system tray for udiskie

--- modules/user/udiskie.nix
+++ modules/user/udiskie.nix
@@ -6,4 +6,12 @@
     enable = true;
     notify = false;
   };
+
+  systemd.user.targets.tray = {
+    # Needed for some services that require tray.
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = [ "graphical-sessionpre.target" ];
+    };
+  };
 }
