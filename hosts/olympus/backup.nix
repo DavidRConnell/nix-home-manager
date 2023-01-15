6970f00c6e0e06a01898a3b5699cbd7d3503904c
David R Connell <davidconnell12@gmail.com>
2023-01-15 12:10:08 -0600

    Get internal SSD for backups
    Move from HDD to SSD. No longer need powermangement for spinning down drive.

--- hosts/olympus/backup.nix
+++ hosts/olympus/backup.nix
@@ -10,24 +10,12 @@ in {
     options = [ "x-systemd.automount" "noauto" ];
   };
 
-  # 241 means 30 min for some reason (see man hdparm).
-  powerManagement.powerUpCommands = ''
-    ${pkgs.hdparm}/sbin/hdparm -S 241 ${device};
-  '';
-
-  # Drive does not power down when the server does otherwise.
-  powerManagement.powerDownCommands = ''
-    ${pkgs.udiskie}/bin/udiskie-umount ${device} --detach
-  '';
-
   services.restic.backups.local = {
     paths = [ "/data" ];
     repository = "${mountpoint}/olympus";
     passwordFile = "/etc/nixos/restic-password";
     initialize = true;
-    timerConfig = {
-      OnBootSec = "20m";
-      OnCalendar = "daily";
-    };
+    extraBackupArgs = [ "--no-scan" ]; # Can't see progress anyway
+    timerConfig = { OnCalendar = "daily"; };
   };
 }
