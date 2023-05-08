2810db3b3c6a4320ee3f821b5571cef1854e9d6f
David R Connell <davidconnell12@gmail.com>
2023-05-08 12:36:53 -0500

    Add fileflows

---
+++ modules/host/fileflows.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+let
+  mkVHost = pkgs.lib.mkVHost;
+  subdomain = "fileflows";
+  port = "9014";
+in {
+  services.nginx.virtualHosts = mkVHost { inherit subdomain port; };
+  virtualisation.oci-containers.containers."${subdomain}" = {
+    autoStart = true;
+    image = "revenz/fileflows:latest";
+    ports = [ "${port}:5000" ];
+    environment = { TempPathHost = "/tmp/fileflows"; };
+    volumes = [
+      "/var/run/docker.sock:/var/run/docker.sock:ro"
+      "/data/fileflows/data:/app/Data"
+      "/data/fileflows/logging:/app/Logs"
+      "/tmp/fileflows:/temp"
+      "/data/fileflows/media:/media"
+      "/data/metube/downloads/audio:/media/audiobooks"
+      "/data/metube/downloads/media:/media/videos"
+    ];
+    extraOptions = [ "--pull=always" ];
+  };
+}
