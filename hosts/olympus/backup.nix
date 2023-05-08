ddd388fd085b09e571a8529adb62429d251737f7
David R Connell <davidconnell12@gmail.com>
2023-05-08 12:37:29 -0500

    Remove pre scan from backup

--- hosts/olympus/backup.nix
+++ hosts/olympus/backup.nix
@@ -15,8 +15,7 @@ in {
     repository = "${mountpoint}/olympus";
     passwordFile = "/etc/nixos/restic-password";
     initialize = true;
-    # NOTE: Added in restic v0.15; nixpkgs on 0.14. Reenable when package bumped.
-    # extraBackupArgs = [ "--no-scan" ]; # Can't see progress anyway
+    extraBackupArgs = [ "--no-scan" ]; # Can't see progress anyway
     timerConfig = { OnCalendar = "06:00"; };
   };
 }
