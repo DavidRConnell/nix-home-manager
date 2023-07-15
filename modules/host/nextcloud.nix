0ad9af0855282f589f0dc68e3016237dbc877c8c
David R Connell <davidconnell12@gmail.com>
2023-07-14 22:48:41 -0500

    Update nextcloud package

--- modules/host/nextcloud.nix
+++ modules/host/nextcloud.nix
@@ -3,7 +3,7 @@
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud26;
+    package = pkgs.nextcloud27;
     hostName = "nextcloud.home";
     home = "/data/nextcloud";
     config.adminpassFile = "${pkgs.writeText "adminpass" "test123"}";
